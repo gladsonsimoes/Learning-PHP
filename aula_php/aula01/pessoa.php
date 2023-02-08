0b3bfd7e482e35e688a8e1066c54c44d6766b670
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-02-08 16:39:57 -0300

    Create pessoa.php

---
+++ aula_php/aula01/pessoa.php
@@ -0,0 +1,28 @@
+<?php
+
+class Pessoa{
+    public $nome = null;
+
+    function __construct($nome){
+        echo 'Objeto criado';
+        $this->nome = $nome;
+    }
+
+    function __get($variavel){
+        echo $this->$variavel;
+    }
+
+    function __set($variavel , $valor){
+        $this->$variavel = $valor;
+    }
+}
+
+$pessoa = new Pessoa("Roberto");
+echo "<hr>";
+echo $pessoa -> __get('nome');
+$pessoa->__set('idade', 22);
+echo "<hr>";
+echo $pessoa -> __get('idade');
+
+
+?>
